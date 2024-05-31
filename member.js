befdb9a82d07696b0bc532852c626c10bdae50fb
John Marcial <johnmarloumarcial@gmail.com>
2024-05-31 18:02:16 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    return {
+        name: 'John Doe',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        greet: function() {
+            console.log('Hello!');
+        }
+    };
+}
