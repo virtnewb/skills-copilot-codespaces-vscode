65dc8942d9d288ac9c263f0325df9803727d9562
John Marcial <johnmarloumarcial@gmail.com>
2024-05-31 18:14:29 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,37 @@
-// Create a web server
-// Load the express module
-var express = require('express');
+// Create web server
+// npm install express
+// npm install body-parser
+// npm install ejs
+// npm install express-sanitizer
+// npm install mongoose
+// npm install method-override
+var express = require("express");
 var app = express();
-var fs = require('fs');
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var Comment = require("./models/comment");
+var seedDB = require("./seeds");
+var methodOverride = require("method-override");
 
-app.use(bodyParser.json());
+// Connect to the database
+mongoose.connect("mongodb://localhost/yelp_camp_v5");
 
-app.get('/comments', function(req, res) {
-    fs.readFile('comments.json', function(err, data) {
-        // Handle file reading
-    });
+// Use body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Use ejs
+app.set("view engine", "ejs");
+
+// Use express-sanitizer
+app.use(methodOverride("_method"));
+
+// Use seedDB
+seedDB();
+
+// Routes
+app.get("/", function(req, res) {
+    res.send("Hello, world!");
 });
+
+// Index
+app
